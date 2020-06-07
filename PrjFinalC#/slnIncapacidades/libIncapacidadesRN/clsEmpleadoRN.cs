c6f06d4bc0ddf3afe99b5848ebc49e2634bd3bb3
dskater07 <66536730+dskater07@users.noreply.github.com>
2020-06-07 16:06:00 -0500

    Empleado

--- PrjFinalC#/slnIncapacidades/libIncapacidadesRN/clsEmpleadoRN.cs
+++ PrjFinalC#/slnIncapacidades/libIncapacidadesRN/clsEmpleadoRN.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using libConexionBd;
+using libLlenarGrids;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 
 namespace libIncapacidadesRN
 {
@@ -21,21 +26,39 @@ namespace libIncapacidadesRN
         DateTime dtFechaUpdate;
         int intDiaCobrar;
         int intDiasProrroga;
-
-
-
+        private SqlParameter[] objDatosMatricula;
+        private clsConexionBd objConex;
+        private clsLlenarGrids objLlenaGrids;
+        private DataSet dsDatos;
+        GridView gvVehiculo;
 
         #endregion
 
         #region "Constructor"
 
+
         #endregion
 
         #region Parámetros
+        public string StrNum_incap { get => strNum_incap; set => strNum_incap = value; }
+        public int IntIdEmpleado { get => intIdEmpleado; set => intIdEmpleado = value; }
+        public int IntIdVehiculo { get => intIdVehiculo; set => intIdVehiculo = value; }
+        public int IdIncapInicial { get => idIncapInicial; set => idIncapInicial = value; }
+        public int IdTipoIncapciad { get => idTipoIncapciad; set => idTipoIncapciad = value; }
+        public DateTime DtFechaInicial { get => dtFechaInicial; set => dtFechaInicial = value; }
+        public DateTime DtFechaFinal { get => dtFechaFinal; set => dtFechaFinal = value; }
+        public double DblIBC { get => dblIBC; set => dblIBC = value; }
+        public string StrObservaciones { get => strObservaciones; set => strObservaciones = value; }
+        public DateTime DtFechaUpdate { get => dtFechaUpdate; set => dtFechaUpdate = value; }
+        public int IntDiaCobrar { get => intDiaCobrar; set => intDiaCobrar = value; }
+        public int IntDiasProrroga { get => intDiasProrroga; set => intDiasProrroga = value; }
+
 
         #endregion
 
+
         #region "Métodos Privados"
+
 
         #endregion
         #region "Métodos Públicos"
